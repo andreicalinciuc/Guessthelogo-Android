b3e191dcaa47267f30f69660a47a5a8e546b55aa
andreicalinciuc <scoala.andrei@yahoo.com>
2020-05-26 15:15:18 +0300

    create view mode

--- app/src/main/java/com/example/guess_the_logo/GameViewModel.java
+++ app/src/main/java/com/example/guess_the_logo/GameViewModel.java
@@ -1,4 +1,25 @@
 package com.example.guess_the_logo;
 
-public class GameViewModel {
+import android.content.res.Resources;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+
+public class GameViewModel extends ViewModel {
+
+    public final MutableLiveData<GameModel> userLiveData = new MutableLiveData<>();
+
+    public GameViewModel(List<Integer> image_list, Resources resources) {
+        userLiveData.setValue(new GameModel(image_list, resources));
+    }
+
+    void chooseCharacter(String character) {
+        // depending on the action, do necessary business logic calls and update the
+        // userLiveData.
+        userLiveData.getValue().chooseCharacter(character);
+
+    }
 }
